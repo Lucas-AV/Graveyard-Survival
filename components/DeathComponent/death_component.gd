7c0b898540c6be3be78e43ab54790fe12a86b8f3
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-11-05 12:52:44 -0300

    Sprite Sheet added + Animation of death corrected

--- components/DeathComponent/death_component.gd
+++ components/DeathComponent/death_component.gd
@@ -8,11 +8,12 @@ func _ready():
 	health_component.died.connect(on_died)
 
 func on_died():
-	if owner == null || not owner is Node2D:
-		return
+	if owner == null || not owner is Node2D: return
 	var spawn_position = owner.global_position
+	
 	var entities = get_tree().get_first_node_in_group("entities_layer")
-	entities.remove_child(self) # Remove o personagem da árvore principal do jogo
-	entities.add_child(self) # Adiciona uma versão baseada no death component
+	get_parent().remove_child(self) # Adiciona uma versão baseada no death component
+	entities.add_child(self)
+	
 	global_position = spawn_position
 	$AnimationPlayer.play("default")
