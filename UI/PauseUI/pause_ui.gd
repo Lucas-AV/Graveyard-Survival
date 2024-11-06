84a891e7089410618e9731994bdac3356d801e30
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2024-11-06 15:47:12 -0300

    settings UI implement

--- UI/PauseUI/pause_ui.gd
+++ UI/PauseUI/pause_ui.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 var is_closing: bool = false
+var is_setting_pressed: bool = false
+var settings_instance
 
 func _ready():
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
@@ -12,9 +14,13 @@ func _ready():
 
 func _unhandled_input(event):
 	if event.is_action_pressed("pause") && !is_closing:
-		close()
+		if(is_setting_pressed):
+			self.visible = true
+			is_setting_pressed = false
+			on_settings_closed(settings_instance)
+		else:
+			close()
 		get_tree().root.set_input_as_handled()
-
 
 func close(): 
 	var tween = create_tween()
@@ -26,16 +32,21 @@ func close():
 	queue_free()
 
 func on_resume(): close()
-func on_restart(): get_tree().reload_current_scene()
+func on_restart(): 
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 
 var settings = preload("res://UI/SettingsUI/settings_ui.tscn")
 func on_settings():
+	is_setting_pressed = true
 	self.visible = false
-	var settings_instance = settings.instantiate()
+	settings_instance = settings.instantiate()
 	add_child(settings_instance)
 	settings_instance.back_pressed.connect(on_settings_closed.bind(settings_instance))
 
 func on_settings_closed(settings_instance: Node):
 	settings_instance.queue_free()
 
-func on_quit(): get_tree().quit()
+func on_quit():
+	get_tree().paused = false 
+	get_tree().change_scene_to_file("res://UI/StartMenuUI/start_menu_ui.tscn")
--- UI/SettingsUI/settings_ui.gd
+++ UI/SettingsUI/settings_ui.gd
@@ -11,26 +11,27 @@ func _ready():
 	%FullscreenCheckbox.pressed.connect(on_fullscreen_pressed)
 
 
-func set_audio_data(name: String, percent: float):
-	var index = AudioServer.get_bus_index(name)
+func set_audio_data(_name: String, percent: float):
+	var index = AudioServer.get_bus_index(_name)
 	var volume = linear_to_db(percent)
 	AudioServer.set_bus_volume_db(index, volume)
 
 
-func on_audio_slider_changed(value: float, name: String):
-	set_audio_data(name, value)
+func on_audio_slider_changed(value: float, _name: String):
+	set_audio_data(_name, value)
 
 func setup_sliders():
-	%MusicSlider.value = get_audio_data("music")
-	%EffectsSlider.value = get_audio_data("effects")
+	pass
+	#%MusicSlider.value = get_audio_data("music")
+	#%EffectsSlider.value = get_audio_data("effects")
 # Setup
 func setup_fullscreen_button_checkbox_pressed():
 	var mode = DisplayServer.window_get_mode()
 	%FullscreenCheckbox.button_pressed = mode == DisplayServer.WINDOW_MODE_FULLSCREEN
 
 # Sliders
-func get_audio_data(name: String):
-	var index = AudioServer.get_bus_index(name)
+func get_audio_data(_name: String):
+	var index = AudioServer.get_bus_index(_name)
 	var volume = AudioServer.get_bus_volume_db(index)
 	return db_to_linear(volume)
 
